ef7edcc55238d1626c98f75cb428a2f771bbc4ea
julia nicole <128698991+julianicoleroese@users.noreply.github.com>
2023-10-24 11:51:15 -0300

    Add files via upload

---
+++ exercicio_8.js
@@ -0,0 +1,6 @@
+    var board = document.getElementById('board');
+    var message = document.getElementById('massage');
+
+    let jogadorAtual = 'X';
+    let cell = Array(9).fill(' ')
+    let gameover = false;
